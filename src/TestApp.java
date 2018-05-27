30c0834c9cea95a7f815cecfbfb54886939edb12
Siddiq Hamed <superfox.2007@yahoo.com>
2018-05-27 11:51:57 +0200

    modified the pp to do some calculations

--- src/TestApp.java
+++ src/TestApp.java
@@ -3,6 +3,13 @@ public class TestApp {
 	public static void main (String agrs[])
 	{
 		System.out.println("Hello world");
+		int age = 13 ;
+		int birthYear = 2000 ;
+		
+		int thisYear = age + birthYear ;
+		
+		System.out.println("the result = "+thisYear);
+		
 	}
 
 }
