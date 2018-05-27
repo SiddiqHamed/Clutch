138601eebca7f3f207ca3b3929cb9e80c069af83
Siddiq Hamed <superfox.2007@yahoo.com>
2018-05-27 12:15:57 +0200

    another modification

--- src/TestApp.java
+++ src/TestApp.java
@@ -6,10 +6,15 @@ public class TestApp {
 		int age = 13 ;
 		int birthYear = 2000 ;
 		
-		int thisYear = age + birthYear ;
+		int thisYear = calcYear( age , birthYear);
 		
 		System.out.println("the result = "+thisYear);
 		
+	}
+	
+	public static int calcYear(int x , int y)
+	{
+		return x + y ;
 	}
 
 }
