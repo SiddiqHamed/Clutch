05269af71f1a3c5a2ea118ffbcb55b5cced3cbb5
SiddiqHamed <39650079+SiddiqHamed@users.noreply.github.com>
2019-12-25 18:07:40 -1000

    Update TestApp.java

--- src/TestApp.java
+++ src/TestApp.java
@@ -2,29 +2,6 @@
 public class TestApp {
 	public static void main (String agrs[])
 	{
-		System.out.println("Hello world");
-		int age = 13 ;
-		int birthYear = 2000 ;
-		
-		int thisYear = calcYear( age , birthYear);
-		
-		System.out.println("the result = "+thisYear);
-		System.out.println("I am actully the master branch");
-		
+		System.out.println("Hello world");	
 	}
-	
-	public static int calcYear(int x , int y)
-	{
-		return x + y ;
-	}
-	
-	public static int calcYear(int x , int y , int z)
-	{
-		return x + y ;
-	}
-	public static int calcYear(int x , int y , int z , int c)
-	{
-		return x + y ;
-	}
-
 }
